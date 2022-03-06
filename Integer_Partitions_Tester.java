1575e79e3db6624219001e53bf0d4c6cc60f6e2b
Aditi Hiremath <aditihiremath@wireless-10-148-116-45.public.utexas.edu>
2022-03-06 17:50:43 -0600

    fibonacci added

--- Integer_Partitions_Tester.java
+++ Integer_Partitions_Tester.java
@@ -1,13 +1,22 @@
 public class Integer_Partitions_Tester {
     public static void main(String[] args) {
         Integer_Partitions n = new Integer_Partitions();
-        for (int i = 1; i < 20; i++) {
-            n.printAllUniqueParts(i);
-            System.out.println(i); 
+        int[] primeNums = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 87, 93, 97};
+        //for (int i = 1; i < 10000000; i++) {
+        for (int i = 0; i < primeNums.length; i++) {
+            n.printAllUniqueParts(primeNums[i]);
+            System.out.println(primeNums[i]); 
             n.numPartitions();
-            n.oddPartitions(); // Theorem 3
+            //n.oddPartitions(); // Theorem 3
             //n.distinctPartitions(); //TODO
             System.out.println();
         }
+    }
+
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
     }
 }
